c1b3441591fd76a5c1aaa3f8e965cf09d9980a88
Agnieszka Pastuszczak <agnieszka.pastuszczak@xfive.co>
2022-05-26 13:56:59 +0200

    [OPLU-24] Use DEFAULT_SENDER value as mail sender for Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Devise.setup do |config|
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = ENV.fetch('DEFAULT_SENDER', nil)
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
