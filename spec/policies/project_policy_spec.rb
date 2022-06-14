106c8dacd6f10a49ec6cef5e5983f1696d35ea72
Agnieszka Pastuszczak <agnieszka.pastuszczak@xfive.co>
2022-06-14 09:35:58 +0200

    [OPLU-43] Add ProjectPolicy

---
+++ spec/policies/project_policy_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProjectPolicy do
+  subject(:record_policy) { described_class }
+
+  permissions :create? do
+    context 'when logged in admin' do
+      context 'when admin is approved' do
+        let(:current_user) { build(:user, :admin, :approved) }
+
+        it 'grants access' do
+          expect(record_policy).to permit(current_user, build(:project))
+        end
+      end
+
+      context 'when admin is not approved' do
+        let(:current_user) { build(:user, :admin, :not_approved) }
+
+        it 'denies access' do
+          expect(record_policy).not_to permit(current_user, build(:project))
+        end
+      end
+    end
+
+    context 'when not logged in' do
+      let(:current_user) { nil }
+
+      it 'denies access' do
+        expect(record_policy).not_to permit(current_user, build(:project))
+      end
+    end
+  end
+
+  permissions :index?, :show?, :new?, :update?, :edit?, :destroy? do
+    context 'when logged in admin' do
+      context 'when admin is approved' do
+        let(:current_user) { build(:user, :admin, :approved) }
+
+        it 'denies access' do
+          expect(record_policy).not_to permit(current_user, build(:project))
+        end
+      end
+
+      context 'when admin is not approved' do
+        let(:current_user) { build(:user, :admin, :not_approved) }
+
+        it 'denies access' do
+          expect(record_policy).not_to permit(current_user, build(:project))
+        end
+      end
+    end
+
+    context 'when not logged in' do
+      let(:current_user) { nil }
+
+      it 'denies access' do
+        expect(record_policy).not_to permit(current_user, build(:project))
+      end
+    end
+  end
+end
