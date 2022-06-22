e242399d8fb76be4a93f354579530e78b780df9b
Agnieszka Pastuszczak <agnieszka.pastuszczak@xfive.co>
2022-06-22 15:04:27 +0200

    [OPLU-118] Add comment stating that SessionTOkens#create would generate new JWT token and return it in headers

--- app/controllers/ver1/session_tokens_controller.rb
+++ app/controllers/ver1/session_tokens_controller.rb
@@ -4,6 +4,8 @@ module Ver1
   class SessionTokensController < ApiApplicationController
     before_action :require_login
 
+    # the new token will be available in response headers
+    # for more details see: config/initializers/devise.rb
     def create
       authorize :session_token, :create?
 
