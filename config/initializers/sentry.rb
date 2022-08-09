dacc50eeebf972d4a1c57cbe51a52577ec2f3475
Agnieszka Pastuszczak <agnieszka@xfive.co>
2022-08-09 08:08:28 +0200

    Update Sentry traces sampler config (#62)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,9 @@ Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.environment = ENV.fetch('ENVIRONMENT_NAME', 'production')
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.traces_sampler = lambda do |_context|
-    true
+  config.traces_sampler = lambda do |sampling_context|
+    next sampling_context[:parent_sampled] unless sampling_context[:parent_sampled].nil?
+
+    0.2
   end
 end
