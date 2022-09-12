b06116d12199d602a41da2f39277a40660bea383
annki <yahyush@gmail.com>
2022-09-13 00:24:09 +0200

    test fixes

--- spec/requests/v1/comments_spec.rb
+++ spec/requests/v1/comments_spec.rb
@@ -5,8 +5,11 @@ require 'swagger_helper'
 RSpec.describe 'v1/comments', type: :request do
   path '/api/v1/projects/{project_id}/tokens/{token_id}/comments' do
     let(:user) { create(:user, :admin, :approved) }
+    let(:user_id) { user.id }
     let(:project) { create(:project) }
+    let(:project_id) { project.id }
     let(:token) { create(:token, project:) }
+    let(:token_id) { token.id }
 
     get('Retrieves all the comments for given token') do
       tags 'Projects'
