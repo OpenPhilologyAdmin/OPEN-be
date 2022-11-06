8cc64dc1126ede4f52a9d94ee722f4fdb4975aaf
annki <yahyush@gmail.com>
2022-11-07 00:54:56 +0100

    Remove code bloopers

--- app/services/witnesses_manager/base.rb
+++ app/services/witnesses_manager/base.rb
@@ -10,9 +10,6 @@ module WitnessesManager
       @params = params
       @user = user
       @witness = @project.find_witness!(siglum)
-
-      update_last_editor(user:, project:)
-      update_last_edited_project(project:, user:)
     end
 
     def self.perform(project:, siglum:, user:, params: {})
