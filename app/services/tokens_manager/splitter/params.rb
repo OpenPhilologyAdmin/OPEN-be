c711debe6043a47cbb6af8ee6a59f4d296c6bb33
annki <yahyush@gmail.com>
2023-01-16 00:16:24 +0100

    validations + test

--- app/services/tokens_manager/splitter/params.rb
+++ app/services/tokens_manager/splitter/params.rb
@@ -5,21 +5,39 @@ module TokensManager
     class Params
       include ActiveModel::Model
 
-      attr_reader :project, :new_variants
+      attr_reader :project, :new_variants, :source_token
 
-      validates :project, presence: true
+      validates :project,      presence: true
+      validates :new_variants, presence: true
+      validates :source_token, presence: true
 
-      def initialize(project:, new_variants:)
-        @project = project
+      validate :validate_new_variants
+
+      SPLITTER_PHRASE = '{scissors}'
+
+      def initialize(project:, new_variants:, source_token:)
+        @project      = project
         @new_variants = new_variants
+        @source_token = source_token
       end
 
       private
 
       def validate_new_variants
-        # TO DO: If any variant of new variants doesn't include {scissors} throw an error
+        return errors.add(:new_variants, :blank) if new_variants.blank?
 
-        # return error.add(:new_variants, :unprocessable_entity)
+        new_variants.each do |variant|
+          return errors.add(:new_variants, :splitter_phrase_missing) if variant[:t].exclude?(SPLITTER_PHRASE)
+          return errors.add(:new_variants, :invalid_variant_text)    if source_token_ts.exclude?(plain_t(variant[:t]))
+        end
+      end
+
+      def source_token_ts
+        @source_token_ts ||= source_token.variants.pluck(:t)
+      end
+
+      def plain_t(new_variant_t)
+        new_variant_t.split(SPLITTER_PHRASE).join
       end
     end
   end
